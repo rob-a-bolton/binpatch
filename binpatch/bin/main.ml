a1e28b0da6792e2116e9cdfe245cadef1422805b
rob <rob.a.bolton@gmail.com>
2021-03-15 15:30:17 +0000

    Handles patches as ranges, multiple ranges per patch file

--- binpatch/bin/main.ml
+++ binpatch/bin/main.ml
@@ -1,8 +1,16 @@
 open Cmdliner;;
 open Binpatch.Patch;;
 
-type create_error_reason = No_src | No_dst | No_off | No_len
-exception Create_error of create_error_reason
+let range_scanner arg_str =
+  try
+    Ok (Scanf.sscanf arg_str "%d:%d" (fun x y -> (x, y)))
+  with Scanf.Scan_failure e -> Error (`Msg e)
+
+let range_printer formatter (x, y) =
+  Format.fprintf formatter "%d:%d" x y
+
+let range_conv =
+  Arg.(conv (range_scanner, range_printer))
 
 type cmd_mode =
   | Create
@@ -24,45 +32,29 @@ let c_dst =
   let doc = "Destination file" in
   Arg.(value & opt (some string) None & info ["d"; "dst"] ~docv:"DST" ~doc)
 
-let c_off =
-  let doc = "Offset in binary input file" in
-  Arg.(value & opt (some int) None & info ["o"; "off"] ~docv:"OFF" ~doc)
-
-let c_len =
-  let doc = "Length" in
-  Arg.(value & opt (some int) None & info ["l"; "len"] ~docv:"LEN" ~doc)
+let c_ranges =
+  let doc = "Patch data range" in
+  Arg.(value & (pos_all range_conv []) & info [] ~docv:"RANGE" ~doc)
 
 let c_mode =
   Arg.(last & vflag_all [None] [c_create; c_apply])
 
-(*
-  Check flags exist: src, dst, off, len
-  Check src file exists, and is readable
-  Check 0 < off < off + len < len(file)
- *)
-
-let validate_create_flags src dst off len =
-  match (src, dst, off, len) with
-  | (Some src, Some dst, Some off, Some len) -> Ok (src, dst, off, len)
-  | (None, _, _, _) -> Error "No_src"
-  | (_, None, _, _) -> Error "No_dst"
-  | (_, _, None, _) -> Error "No_off"
-  | (_, _, _, None) -> Error "No_len"
+let validate_create_flags src dst =
+  match (src, dst) with
+  | (Some src, Some dst) -> Ok (src, dst)
+  | (None, _) -> Error "No_src"
+  | (_, None) -> Error "No_dst"
 
 let validate_apply_flags src dst =
   match (src, dst) with
   | (Some src, Some dst) -> Ok (src, dst)
   | (None, _) -> Error "No_src"
   | (_, None) -> Error "No_dst"
-  (* | (None, _, _, _) -> Error No_src
-   * | (_, None, _, _) -> Error No_dst
-   * | (_, _, None, _) -> Error No_off
-   * | (_, _, _, None) -> Error No_len *)
 
-let try_create src dst off len =
-  match Result.bind (validate_create_flags src dst off len)
-          (fun (src, dst, off, len) ->
-            create_patch src dst off len)
+let try_create src dst ranges =
+  match Result.bind (validate_create_flags src dst)
+          (fun (src, dst) ->
+            create_patch src dst ranges)
   with
   | Ok msg -> `Ok msg
   | Error msg -> `Error (false, msg)
@@ -78,10 +70,9 @@ let try_apply src dst =
 let run_cmd (mode:cmd_mode option)
             (src:string option)
             (dst:string option)
-            (off:int option)
-            (len:int option) =
+            (ranges:(int * int) list) =
   match mode with
-  | Some Create -> try_create src dst off len
+  | Some Create -> try_create src dst ranges
   | Some Apply -> try_apply src dst
   | None -> `Error (false, "Must supply a command")
 
@@ -90,11 +81,9 @@ let main =
   let man = [
       `S Manpage.s_description;
       `P "$(tname) can create binary patch files from a source file, offset, and length";
-      `P "Use either --create or --apply to create or apply patches";
-      `P "  $(b,create) - Creates a patch file. Requires: $(b,src), $(b,off), $(b,len)";
-      `P "  $(b,apply) - Applies a patch file. Requires: $(b,src), $(b,dst)";]
+      `P "Use either --create or --apply to create or apply patches";]
   in
-  Term.(ret (const run_cmd $ c_mode $ c_src $ c_dst $ c_off $ c_len)),
+  Term.(ret (const run_cmd $ c_mode $ c_src $ c_dst $ c_ranges)),
   Term.info "binpatch" ~version:"v0.0.1" ~doc ~exits:Term.default_exits ~man;;
 
 let () =
--- binpatch/lib/patch.ml
+++ binpatch/lib/patch.ml
@@ -1,26 +1,41 @@
 let buffer_size = 1024 * 8
 
-let create_patch src dst off len =
+let range_validator file_size =
+  fun (off, len) -> off + len <= file_size
+
+let range_valid file_size (off, len) =
+  (range_validator file_size) (off, len)
+
+let can_patch file_size ranges =
+  let valid_ranges =
+    List.filter (range_validator file_size) ranges
+  in
+  List.length valid_ranges <> List.length ranges
+
+let create_patch src dst ranges =
   if not (Sys.file_exists src) then
     Error "Src file does not exist"
   else if (Sys.is_directory src) then
     Error "Src is not a file"
-  else if (Unix.stat src).st_size < (off + len) then
-    Error "File not large enough for specified patch"
+  else if can_patch (Unix.stat src).st_size ranges then
+    Error "File not large enough for specified patch[es]"
   else
   let in_channel = open_in src in
   let out_channel = open_out dst in
   let buffer = Bytes.create buffer_size in
-  ignore (seek_in in_channel off);
-  output_binary_int out_channel off;
-  output_binary_int out_channel len;
-  let rec copy_loop bytes_read =
+  let rec copy_loop bytes_read len =
     match input in_channel buffer 0 (min (len - bytes_read) buffer_size) with
     | 0 -> ()
-    | r -> ignore (output out_channel buffer 0 r); copy_loop (bytes_read + r)
+    | r -> ignore (output out_channel buffer 0 r); copy_loop (bytes_read + r) len
+  in
+  let stream_patch (off, len) =
+    output_binary_int out_channel off;
+    output_binary_int out_channel len;
+    seek_in in_channel off;
+    copy_loop 0 len
   in
   try
-    copy_loop 0;
+    List.iter stream_patch ranges;
     flush out_channel;
     close_in in_channel;
     close_out out_channel;
@@ -30,6 +45,13 @@ let create_patch src dst off len =
     close_out_noerr out_channel;
     Unix.unlink dst;
     Error "Write error"
+
+let get_patch_header in_chan =
+  try
+    let off = input_binary_int in_chan in
+    let len = input_binary_int in_chan in
+    Some (off, len)
+  with End_of_file -> None
 
 let apply_patch src dst =
   if not (Sys.file_exists src) then
@@ -42,27 +64,31 @@ let apply_patch src dst =
     Error "Dst is not a file"
   else
     let in_channel = open_in src in
-    let off = input_binary_int in_channel in
-    let len = input_binary_int in_channel in
-    if (Unix.stat dst).st_size < (off + len) then
-      Error "File not large enough for specified patch"
-    else
-      let out_channel = open_out_gen [Open_wronly; Open_binary] 0 dst in
-      let buffer = Bytes.create buffer_size in
-      ignore (seek_out out_channel off);
-      let rec copy_loop bytes_read =
-        match input in_channel buffer 0 (min (len - bytes_read) buffer_size) with
-        | 0 -> ()
-        | r -> ignore (output out_channel buffer 0 r); copy_loop (bytes_read + r)
-      in
-      try
-        copy_loop 0;
-        flush out_channel;
-        close_in in_channel;
-        close_out out_channel;
-        Ok "Patched file"
-      with _ ->
-        close_in_noerr in_channel;
-        close_out_noerr out_channel;
-        Unix.unlink dst;
-        Error "Write error"
+    let out_channel = open_out_gen [Open_wronly; Open_binary] 0 dst in
+    let buffer = Bytes.create buffer_size in
+    let rec copy_loop bytes_read len =
+      match input in_channel buffer 0 (min (len - bytes_read) buffer_size) with
+      | 0 -> ()
+      | r -> ignore (output out_channel buffer 0 r); copy_loop (bytes_read + r) len
+    in
+    let rec apply_loop () =
+      match get_patch_header in_channel with
+      | None -> ()
+      | Some (off, len)
+        -> begin
+          seek_out out_channel off;
+          copy_loop 0 len;
+          apply_loop ()
+        end
+    in
+    try
+      apply_loop ();
+      flush out_channel;
+      close_in in_channel;
+      close_out out_channel;
+      Ok "Patched file"
+    with _ ->
+      close_in_noerr in_channel;
+      close_out_noerr out_channel;
+      Unix.unlink dst;
+      Error "Write error"
